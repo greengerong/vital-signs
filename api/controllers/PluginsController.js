03f1cc797ef48ee033efa9e4d8aa4275c8913c3f
=greengerong <=grzx2210@163.com>
2013-06-10 17:56:04 +0800

    [zhaxi] list all plugins service

--- api/controllers/PluginsController.js
+++ api/controllers/PluginsController.js
@@ -1,23 +1,34 @@
 var fs = require("fs"), unzip = require("unzip");
 
+var _pluginsDir = './plugins/';
 var PluginsController = {
 
-    _unzip:function (tmp_path, name, target_path) {
-        fs.createReadStream(tmp_path)
-            .on('close',function () {
-                sails.log("plguins " + name + " upload finished.");
-            }).pipe(unzip.Extract({ path:target_path }));
-    },
+        _unzip:function (tmp_path, name, target_path) {
+            fs.createReadStream(tmp_path)
+                .on('close',function () {
+                    sails.log("plguins " + name + " upload finished.");
+                }).pipe(unzip.Extract({ path:target_path }));
+        },
 
-    // To trigger this action locally, visit: `http://localhost:port/plugins/upload`
-    upload:function (req, res) {
-        var tmp_path = req.files.thumbnail.path;
-        var fileName = req.files.thumbnail.name;
-        var name = fileName.substr(0, fileName.lastIndexOf("."));
-        var target_path = './plugins/' + name;
-        this._unzip(tmp_path, name, target_path);
-        res.view();
+        upload:function (req, res) {
+            var tmp_path = req.files.thumbnail.path;
+            var fileName = req.files.thumbnail.name;
+            var name = fileName.substr(0, fileName.lastIndexOf("."));
+            var target_path = _pluginsDir + name;
+            this._unzip(tmp_path, name, target_path);
+            res.view();
+        },
+
+        all:function (req, res) {
+
+            fs.readdir(_pluginsDir, function (error, list) {
+                if (error) {
+                    res.send(500, error);
+                }
+                res.send(list, 200);
+            })
+        }
+
     }
-
-};
+    ;
 module.exports = PluginsController;
