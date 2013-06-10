7bbbb0536f64323055147c74202bbcbb7aa0ea82
=greengerong <=grzx2210@163.com>
2013-06-11 01:08:41 +0800

    [zhaxi] customer middle ware

--- config/application.js
+++ config/application.js
@@ -1,30 +1,43 @@
 module.exports = {
-	
-	// Name of the application (used as default <title>)
-	appName: "Sails Application",
 
-	// Port this Sails application will live on
-	port: 1337,
+    // Name of the application (used as default <title>)
+    appName:"vital-signs",
 
-	// The environment the app is deployed in 
-	// (`development` or `production`)
-	//
-	// In `production` mode, all css and js are bundled up and minified
-	// And your views and templates are cached in-memory.  Gzip is also used.
-	// The downside?  Harder to debug, and the server takes longer to start.
-	environment: 'development',
+    // Port this Sails application will live on
+    port:1337,
 
-	// Logger
-	// Valid `level` configs:
-	// 
-	// - error
-	// - warn
-	// - debug
-	// - info
-	// - verbose
-	//
-	log: {
-		level: 'info'
-	}
+    // The environment the app is deployed in
+    // (`development` or `production`)
+    //
+    // In `production` mode, all css and js are bundled up and minified
+    // And your views and templates are cached in-memory.  Gzip is also used.
+    // The downside?  Harder to debug, and the server takes longer to start.
+    environment:'development',
+
+    // Logger
+    // Valid `level` configs:
+    //
+    // - error
+    // - warn
+    // - debug
+    // - info
+    // - verbose
+    //
+    log:{
+        level:'info'
+    },
+
+    express:{
+        customMiddleware:function (app) {
+
+            app.use(function (err, req, res, next) {
+                if (req.xhr) {
+                    res.send(500, err);
+                    return;
+                }
+                next([err])
+            });
+        }
+    }
 
 };
