d8e87c95332b7635ce679f26e28e76b5d9cca39b
=greengerong <=grzx2210@163.com>
2013-06-13 23:17:52 +0800

    [zhaxi] add nav

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ manageApp.
     config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
         when('/plugins', {templateUrl:'/templates/plugins.html', controller:"pluginsCtr"}).
+        when('/npm', {templateUrl:'/templates/npm.html', controller:"npmCtr"}).
         otherwise({redirectTo:'/plugins'});
 }]).factory("underscore", ["$window", function ($window) {
     return $window._;
@@ -10,28 +11,38 @@ manageApp.
 
 var pluginsCtr = function ($scope, $http, $timeout) {
 
-        var getDescription = function (plugins) {
-            angular.forEach(plugins, function (item) {
-                $http.get("/plugins/about?name=" + item.key).success(
-                    function (item) {
-                        return function (data) {
-                            $timeout(function () {
-                                item.desc = data;
-                            });
-                        };
-                    }(item));
-            });
-        };
-
-        $http.get("/plugins/allPlugins").success(function (data) {
-            $timeout(function () {
-                $scope.plugins = data.map(function (item) {
-                    return {key:item};
-                });
-                getDescription($scope.plugins);
-            });
+    var getDescription = function (plugins) {
+        angular.forEach(plugins, function (item) {
+            $http.get("/plugins/about?name=" + item.key).success(
+                function (item) {
+                    return function (data) {
+                        $timeout(function () {
+                            item.desc = data;
+                        });
+                    };
+                }(item));
         });
-    }
-    ;
+    };
+
+    $http.get("/plugins/allPlugins").success(function (data) {
+        $timeout(function () {
+            $scope.plugins = data.map(function (item) {
+                return {key:item};
+            });
+            getDescription($scope.plugins);
+        });
+    });
+};
+
+var npmCtr = function ($scope, $http, $timeout) {
+
+};
+
+var navCtr = function ($scope, $location) {
+    $scope.active = $location.$$path;
+};
+
 manageApp.controller("pluginsCtr", ["$scope", "$http", "$timeout", pluginsCtr]);
+manageApp.controller("npmCtr", ["$scope", "$http", "$timeout", npmCtr]);
+manageApp.controller("navCtr", ["$scope", "$location", navCtr]);
 
